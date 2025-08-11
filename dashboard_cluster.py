2ef458a3b7ca2f4dc23a2dbbfe7a966abcfc9a82
emiliobneto <emilio.bneto@usp.br>
2025-08-11 13:59:43 -0300

    Update dashboard_cluster.py

--- dashboard_cluster.py
+++ dashboard_cluster.py
@@ -40,7 +40,7 @@ except Exception:
 st.set_page_config(
     page_title="Dashboard de Análise de Clusters para o Município de São Paulo",
     page_icon="📊",
-    layout="",
+    layout="wide",
     initial_sidebar_state="collapsed",
 )
 
@@ -846,6 +846,7 @@ else:
                 st.markdown("**Clusters**")
                 gtmp = gdf_cluster if not cats_sel else gdf_cluster[gdf_cluster[cluster_col].isin(cats_sel)]
                 st.dataframe(gtmp.drop(columns=gtmp.geometry.name, errors='ignore').head(1000), use_container_width=True)
+
 
 
 
